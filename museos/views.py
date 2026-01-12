834437f7a5552a30f41e8ee303734f60658086c2
Cristian Bravo <105746182+crash8992@users.noreply.github.com>
2026-01-12 21:03:38 +0000

    aplicación de frontend

--- museos/views.py
+++ museos/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Museo
 
-# Create your views here.
+def index(request):
+    # PASO 1: Creamos la variable y le ponemos el nombre 'mis_museos'
+    mis_museos = Museo.objects.all()
+    
+    # PASO 2: Preparamos el paquete. 
+    # La clave 'lista_museos' es como se llamará en el HTML.
+    # El valor 'mis_museos' debe coincidir EXACTAMENTE con el nombre del PASO 1.
+    contexto = {
+        'lista_museos': mis_museos 
+    }
+    
+    return render(request, 'museos/index.html', contexto)
